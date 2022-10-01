8d12c6e4bf0e9d909cd98e7a4e7ab3d05fd8d739
grand7070 <grand7070@naver.com>
2022-10-01 15:22:40 +0900

    fix : queryDSL 에러 수정

---
+++ src/main/java/com/depromeet/breadmapbackend/domain/review/ReviewImage.java
@@ -0,0 +1,45 @@
+package com.depromeet.breadmapbackend.domain.review;
+
+import com.depromeet.breadmapbackend.domain.bakery.Bakery;
+import com.depromeet.breadmapbackend.domain.common.BaseEntity;
+import com.depromeet.breadmapbackend.domain.common.ImageType;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewImage extends BaseEntity {
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "bakery_id")
+    private Bakery bakery;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ImageType imageType;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Builder
+    public ReviewImage(Review review, Bakery bakery, ImageType imageType, String image) {
+        this.review = review;
+        this.bakery = bakery;
+        this.imageType = imageType;
+        this.image = image;
+    }
+}
