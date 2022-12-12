2e935d67c5e8f252502d0e52fec0a65ce94c3dd0
grand7070 <grand7070@naver.com>
2022-12-12 15:24:51 +0900

    refactor : 깃발 디폴트 색 추가 및 선택 불가 로직 추가

---
+++ src/main/java/com/depromeet/breadmapbackend/domain/flag/exception/FlagColorException.java
@@ -0,0 +1,5 @@
+package com.depromeet.breadmapbackend.domain.flag.exception;
+
+public class FlagColorException extends RuntimeException {
+    public FlagColorException() { super("Incorrect flag color"); }
+}
