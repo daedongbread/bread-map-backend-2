1db75ada4fd85456ace68d49be5c9201309b3a2e
grand7070 <grand7070@naver.com>
2023-04-07 00:00:13 +0900

    refactor : SecurityConfig 권한 수정

--- src/main/java/com/depromeet/breadmapbackend/domain/admin/AdminController.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/admin/AdminController.java
@@ -10,7 +10,6 @@ import com.depromeet.breadmapbackend.domain.user.dto.ReissueRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
