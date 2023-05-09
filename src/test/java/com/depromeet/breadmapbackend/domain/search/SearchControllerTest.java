8368605a5c8c99009e366586bc348044e497cdf7
grand7070 <grand7070@naver.com>
2023-05-09 17:30:48 +0900

    refactor : 검색 띄어쓰기 제거 가능, 검색 API 일부 숨김

--- src/test/java/com/depromeet/breadmapbackend/domain/search/SearchControllerTest.java
+++ src/test/java/com/depromeet/breadmapbackend/domain/search/SearchControllerTest.java
@@ -61,29 +61,29 @@ class SearchControllerTest extends ControllerTest {
         userRepository.deleteAllInBatch();
     }
 
-    @Test
-    void autoComplete() throws Exception {
-        mockMvc.perform(get("/v1/search/auto?word=ba&latitude=37.560992&longitude=127.044174")
-                .header("Authorization", "Bearer " + token.getAccessToken()))
-                .andDo(print())
-                .andDo(document("v1/search/auto",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestHeaders(headerWithName("Authorization").description("유저의 Access Token")),
-                        requestParameters(
-                                parameterWithName("word").description("검색어"),
-                                parameterWithName("latitude").description("중앙 위도"),
-                                parameterWithName("longitude").description("중앙 경도")
-                        ),
-                        responseFields(
-                                fieldWithPath("data.[].bakeryId").description("빵집 고유 번호"),
-                                fieldWithPath("data.[].bakeryName").description("빵집 이름"),
-                                fieldWithPath("data.[].reviewNum").description("빵집 리뷰 갯수"),
-                                fieldWithPath("data.[].distance").description("빵집까지 거리")
-                        )
-                ))
-                .andExpect(status().isOk());
-    }
+//    @Test
+//    void autoComplete() throws Exception {
+//        mockMvc.perform(get("/v1/search/auto?word=ba&latitude=37.560992&longitude=127.044174")
+//                .header("Authorization", "Bearer " + token.getAccessToken()))
+//                .andDo(print())
+//                .andDo(document("v1/search/auto",
+//                        preprocessRequest(prettyPrint()),
+//                        preprocessResponse(prettyPrint()),
+//                        requestHeaders(headerWithName("Authorization").description("유저의 Access Token")),
+//                        requestParameters(
+//                                parameterWithName("word").description("검색어"),
+//                                parameterWithName("latitude").description("중앙 위도"),
+//                                parameterWithName("longitude").description("중앙 경도")
+//                        ),
+//                        responseFields(
+//                                fieldWithPath("data.[].bakeryId").description("빵집 고유 번호"),
+//                                fieldWithPath("data.[].bakeryName").description("빵집 이름"),
+//                                fieldWithPath("data.[].reviewNum").description("빵집 리뷰 갯수"),
+//                                fieldWithPath("data.[].distance").description("빵집까지 거리")
+//                        )
+//                ))
+//                .andExpect(status().isOk());
+//    }
 
     @Test
     void search() throws Exception {
@@ -109,48 +109,48 @@ class SearchControllerTest extends ControllerTest {
                 .andExpect(status().isOk());
     }
 
-    @Test
-    void recentKeywords() throws Exception {
-        mockMvc.perform(get("/v1/search/keywords")
-                .header("Authorization", "Bearer " + token.getAccessToken()))
-                .andDo(print())
-                .andDo(document("v1/search/keywords",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestHeaders(headerWithName("Authorization").description("유저의 Access Token")),
-                        responseFields(
-                                fieldWithPath("data").description("최근 검색어")
-                        )
-                ))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void deleteRecentKeyword() throws Exception {
-        mockMvc.perform(delete("/v1/search/keywords?keyword=bakery")
-                .header("Authorization", "Bearer " + token.getAccessToken()))
-                .andDo(print())
-                .andDo(document("v1/search/keywords/delete",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestHeaders(headerWithName("Authorization").description("유저의 Access Token")),
-                        requestParameters(
-                                parameterWithName("keyword").description("삭제 키워드")
-                        )
-                ))
-                .andExpect(status().isNoContent());
-    }
-
-    @Test
-    void deleteRecentKeywordAll() throws Exception {
-        mockMvc.perform(delete("/v1/search/keywords/all")
-                .header("Authorization", "Bearer " + token.getAccessToken()))
-                .andDo(print())
-                .andDo(document("v1/search/keywords/deleteAll",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestHeaders(headerWithName("Authorization").description("유저의 Access Token"))
-                ))
-                .andExpect(status().isNoContent());
-    }
+//    @Test
+//    void recentKeywords() throws Exception {
+//        mockMvc.perform(get("/v1/search/keywords")
+//                .header("Authorization", "Bearer " + token.getAccessToken()))
+//                .andDo(print())
+//                .andDo(document("v1/search/keywords",
+//                        preprocessRequest(prettyPrint()),
+//                        preprocessResponse(prettyPrint()),
+//                        requestHeaders(headerWithName("Authorization").description("유저의 Access Token")),
+//                        responseFields(
+//                                fieldWithPath("data").description("최근 검색어")
+//                        )
+//                ))
+//                .andExpect(status().isOk());
+//    }
+//
+//    @Test
+//    void deleteRecentKeyword() throws Exception {
+//        mockMvc.perform(delete("/v1/search/keywords?keyword=bakery")
+//                .header("Authorization", "Bearer " + token.getAccessToken()))
+//                .andDo(print())
+//                .andDo(document("v1/search/keywords/delete",
+//                        preprocessRequest(prettyPrint()),
+//                        preprocessResponse(prettyPrint()),
+//                        requestHeaders(headerWithName("Authorization").description("유저의 Access Token")),
+//                        requestParameters(
+//                                parameterWithName("keyword").description("삭제 키워드")
+//                        )
+//                ))
+//                .andExpect(status().isNoContent());
+//    }
+//
+//    @Test
+//    void deleteRecentKeywordAll() throws Exception {
+//        mockMvc.perform(delete("/v1/search/keywords/all")
+//                .header("Authorization", "Bearer " + token.getAccessToken()))
+//                .andDo(print())
+//                .andDo(document("v1/search/keywords/deleteAll",
+//                        preprocessRequest(prettyPrint()),
+//                        preprocessResponse(prettyPrint()),
+//                        requestHeaders(headerWithName("Authorization").description("유저의 Access Token"))
+//                ))
+//                .andExpect(status().isNoContent());
+//    }
 }
