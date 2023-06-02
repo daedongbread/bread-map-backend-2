9f470a5dd3426415ab9143e22965c908dd05864c
jaypark <pjhyun1990@gmail.com>
2023-06-02 16:20:11 +0900

    refactor : noticeService::getNoticeList 실패하는 테스트 추가.

--- src/test/java/com/depromeet/breadmapbackend/domain/notice/NoticeServiceImplTest.java
+++ src/test/java/com/depromeet/breadmapbackend/domain/notice/NoticeServiceImplTest.java
@@ -1,15 +1,23 @@
 package com.depromeet.breadmapbackend.domain.notice;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 
+import com.depromeet.breadmapbackend.domain.notice.dto.NoticeDto;
+import com.depromeet.breadmapbackend.global.dto.PageResponseDto;
 import com.depromeet.breadmapbackend.global.infra.properties.CustomAWSS3Properties;
 import com.depromeet.breadmapbackend.global.infra.properties.CustomFirebaseProperties;
 import com.depromeet.breadmapbackend.utils.TestConfig;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @DataJpaTest
 @Import({
 	NoticeServiceImpl.class,
@@ -26,12 +34,49 @@ class NoticeServiceImplTest {
 
 	@Autowired
 	private NoticeServiceImpl sut;
+	@Autowired
+	private CustomAWSS3Properties customAWSS3Properties;
 
 	@Test
-	void generateMessageTest() throws Exception {
+	@Sql("classpath:notice-test-data.sql")
+	void 알림_타입에_따른_메시지와_이미지_생성_테스트() throws Exception {
+		final String oAuthId = "APPLE_111";
+		final NoticeDayType type = NoticeDayType.BEFORE;
+		final int page = 0;
 
-		sut.getNoticeList("awertwaet", NoticeDayType.BEFORE, 1L, 1);
+		final PageResponseDto<NoticeDto> result = sut.getNoticeList(oAuthId, type, null, page);
 
+		assertThat(result.getContents().stream().map(NoticeDto::getTitle))
+			.containsExactly(
+				"관리자 글이 업데이트 됐어요!",
+				"", "내가 제보한 빵이 추가되었어요!",
+				"내가 제보한 빵집이 추가되었어요!",
+				"내 댓글을 nick_name님이 좋아해요!",
+				"내 댓글에 nick_name님이 대댓글을 달았어요!",
+				"내 리뷰를 nick_name님이 좋아해요!",
+				"nick_name님이 댓글을 달았어요!",
+				"nick_name님이 회원님을 팔로우하기 시작했어요"
+			);
+
+		assertThat(result.getContents().stream().map(NoticeDto::getImage))
+			.containsExactly(
+				"%s/%s.png".formatted(customAWSS3Properties.getCloudFront(),
+					customAWSS3Properties.getDefaultImage().getFlag()),
+				"%s/%s.png".formatted(customAWSS3Properties.getCloudFront(),
+					customAWSS3Properties.getDefaultImage().getFlag()),
+				"%s/%s.png".formatted(customAWSS3Properties.getCloudFront(),
+					customAWSS3Properties.getDefaultImage().getReport()),
+				"%s/%s.png".formatted(customAWSS3Properties.getCloudFront(),
+					customAWSS3Properties.getDefaultImage().getReport()),
+				"%s/%s.png".formatted(customAWSS3Properties.getCloudFront(),
+					customAWSS3Properties.getDefaultImage().getLike()),
+				"%s/%s.png".formatted(customAWSS3Properties.getCloudFront(),
+					customAWSS3Properties.getDefaultImage().getComment()),
+				"%s/%s.png".formatted(customAWSS3Properties.getCloudFront(),
+					customAWSS3Properties.getDefaultImage().getLike()),
+				"%s/%s.png".formatted(customAWSS3Properties.getCloudFront(),
+					customAWSS3Properties.getDefaultImage().getComment()),
+				"image"
+			);
 	}
-
 }
---
+++ src/test/resources/notice-test-data.sql
@@ -0,0 +1,29 @@
+SET
+REFERENTIAL_INTEGRITY FALSE;
+
+TRUNCATE TABLE USER;
+ALTER TABLE USER
+    ALTER COLUMN ID RESTART WITH 1;
+
+TRUNCATE TABLE NOTICE;
+ALTER TABLE NOTICE
+    ALTER COLUMN ID RESTART WITH 1;
+
+
+insert into USER (id, created_at, modified_at, role_type, is_block, is_marketing_info_reception_agreed, is_alarm_on, oauth_type, oauth_id, nick_name, email, gender, image)values
+(111,  '2023-01-01', '2023-01-01', 'USER', false,  true, false, 'APPLE', 'APPLE_111', 'nick_name', 'test@apple.com' , 'MALE', 'image')
+;
+
+
+insert into NOTICE (id, created_at, modified_at, content, content_id, type, from_user_id ,user_id )values
+(111,  '2023-01-01', '2023-01-01', 'content-test1', 1111, 'FOLLOW', 111, 111),
+(112,  '2023-01-02', '2023-01-01', 'content-test2', 1111, 'REVIEW_COMMENT', 111, 111),
+(113,  '2023-01-03', '2023-01-01', 'content-test3', 1111, 'REVIEW_LIKE', 111, 111),
+(114,  '2023-01-04', '2023-01-01', 'content-test4', 1111, 'RECOMMENT', 111, 111),
+(115,  '2023-01-05', '2023-01-01', 'content-test5', 1111, 'REVIEW_COMMENT_LIKE', 111, 111),
+(116,  '2023-01-06', '2023-01-01', 'content-test6', 1111, 'ADD_BAKERY', 111, 111),
+(117,  '2023-01-07', '2023-01-01', 'content-test7', 1111, 'ADD_PRODUCT', 111, 111),
+(118,  '2023-01-08', '2023-01-01', 'content-test8', 1111, 'FLAG_BAKERY_CHANGE', 111, 111),
+(119,  '2023-01-09', '2023-01-01', 'content-test9', 1111, 'FLAG_BAKERY_ADMIN_NOTICE', 111, 111)
+;
+
