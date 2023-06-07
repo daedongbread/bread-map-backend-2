19662a2b3b2fd8777272147da8292a4dfa24e1f8
jaypark <pjhyun1990@gmail.com>
2023-06-07 16:52:51 +0900

    refactor : config embedded redis & redis test 환경 세팅

--- src/main/java/com/depromeet/breadmapbackend/global/infra/EmbeddedRedisConfig.java
+++ src/main/java/com/depromeet/breadmapbackend/global/infra/EmbeddedRedisConfig.java
@@ -1,39 +1,63 @@
 package com.depromeet.breadmapbackend.global.infra;
 
-import com.depromeet.breadmapbackend.global.infra.properties.CustomRedisProperties;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import redis.embedded.RedisServer;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.depromeet.breadmapbackend.global.infra.properties.CustomRedisProperties;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import redis.embedded.RedisServer;
 
 @Slf4j
-@Profile({"default", "local"}) // TODO
+@Profile({"default", "local" }) // TODO
 @Configuration
 @RequiredArgsConstructor
 public class EmbeddedRedisConfig {
-    private final CustomRedisProperties customRedisProperties;
+	private final CustomRedisProperties customRedisProperties;
 
-    private RedisServer redisServer;
+	private RedisServer redisServer;
 
-    @PostConstruct
-    public void redisServer() throws IOException {
-        log.info("Connect to Embedded-Redis");
-        redisServer = RedisServer.builder()
-                .port(customRedisProperties.getPort())
-                .setting("maxmemory 128M")
-                .build();
-        redisServer.start();
-    }
+	@PostConstruct
+	public void redisServer() {
+		log.info("Connect to Embedded-Redis");
+		redisServer = new RedisServer(customRedisProperties.getPort());
+		redisServer.start();
+	}
 
-    @PreDestroy
-    public void stopRedis() {
-        if (redisServer != null && redisServer.isActive()) {
-            redisServer.stop();
-        }
-    }
+	@PreDestroy
+	public void stopRedis() {
+		if (redisServer != null && redisServer.isActive()) {
+			redisServer.stop();
+		}
+	}
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		log.info("Connect to Redis");
+		return new LettuceConnectionFactory(
+			customRedisProperties.getHost(),
+			customRedisProperties.getPort()
+		);  // Lettuce 사용
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setKeySerializer(new StringRedisSerializer());   // Key: String
+		redisTemplate.setValueSerializer(
+			new Jackson2JsonRedisSerializer<>(String.class)
+		);  // Value: 직렬화에 사용할 Object 사용하기
+		return redisTemplate;
+	}
 }
--- src/test/java/com/depromeet/breadmapbackend/utils/ControllerTest.java
+++ src/test/java/com/depromeet/breadmapbackend/utils/ControllerTest.java
@@ -1,40 +1,6 @@
 package com.depromeet.breadmapbackend.utils;
 
-import com.depromeet.breadmapbackend.domain.admin.AdminRepository;
-import com.depromeet.breadmapbackend.domain.auth.AuthService;
-import com.depromeet.breadmapbackend.domain.bakery.BakeryRepository;
-import com.depromeet.breadmapbackend.domain.bakery.report.*;
-import com.depromeet.breadmapbackend.domain.bakery.view.BakeryViewRepository;
-import com.depromeet.breadmapbackend.domain.flag.FlagBakeryRepository;
-import com.depromeet.breadmapbackend.domain.flag.FlagRepository;
-import com.depromeet.breadmapbackend.domain.notice.NoticeRepository;
-import com.depromeet.breadmapbackend.domain.notice.token.NoticeTokenRepository;
-import com.depromeet.breadmapbackend.domain.bakery.product.report.ProductAddReportImageRepository;
-import com.depromeet.breadmapbackend.domain.bakery.product.report.ProductAddReportRepository;
-import com.depromeet.breadmapbackend.domain.bakery.product.ProductRepository;
-import com.depromeet.breadmapbackend.domain.review.ReviewProductRatingRepository;
-import com.depromeet.breadmapbackend.domain.review.ReviewRepository;
-import com.depromeet.breadmapbackend.domain.review.comment.like.ReviewCommentLikeRepository;
-import com.depromeet.breadmapbackend.domain.review.comment.ReviewCommentRepository;
-import com.depromeet.breadmapbackend.domain.review.ReviewImageRepository;
-import com.depromeet.breadmapbackend.domain.review.like.ReviewLikeRepository;
-import com.depromeet.breadmapbackend.domain.review.report.ReviewReportRepository;
-import com.depromeet.breadmapbackend.domain.review.view.ReviewViewRepository;
-import com.depromeet.breadmapbackend.domain.user.block.BlockUserRepository;
-import com.depromeet.breadmapbackend.domain.user.follow.FollowRepository;
-import com.depromeet.breadmapbackend.domain.user.UserRepository;
-import com.depromeet.breadmapbackend.global.infra.EmbeddedRedisConfig;
-import com.depromeet.breadmapbackend.global.infra.properties.CustomAWSS3Properties;
-import com.depromeet.breadmapbackend.global.infra.properties.CustomRedisProperties;
-import com.depromeet.breadmapbackend.global.security.token.JwtTokenProvider;
-import com.depromeet.breadmapbackend.global.S3Uploader;
-import com.depromeet.breadmapbackend.domain.admin.AdminService;
-import com.depromeet.breadmapbackend.domain.bakery.BakeryService;
-import com.depromeet.breadmapbackend.domain.flag.FlagService;
-import com.depromeet.breadmapbackend.domain.review.ReviewService;
-import com.depromeet.breadmapbackend.domain.user.UserService;
-import com.depromeet.breadmapbackend.global.security.token.RedisTokenUtils;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -48,131 +14,177 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.depromeet.breadmapbackend.domain.admin.AdminRepository;
+import com.depromeet.breadmapbackend.domain.admin.AdminService;
+import com.depromeet.breadmapbackend.domain.auth.AuthService;
+import com.depromeet.breadmapbackend.domain.bakery.BakeryRepository;
+import com.depromeet.breadmapbackend.domain.bakery.BakeryService;
+import com.depromeet.breadmapbackend.domain.bakery.product.ProductRepository;
+import com.depromeet.breadmapbackend.domain.bakery.product.report.ProductAddReportImageRepository;
+import com.depromeet.breadmapbackend.domain.bakery.product.report.ProductAddReportRepository;
+import com.depromeet.breadmapbackend.domain.bakery.report.BakeryAddReportImageRepository;
+import com.depromeet.breadmapbackend.domain.bakery.report.BakeryAddReportRepository;
+import com.depromeet.breadmapbackend.domain.bakery.report.BakeryReportImageRepository;
+import com.depromeet.breadmapbackend.domain.bakery.report.BakeryUpdateReportImageRepository;
+import com.depromeet.breadmapbackend.domain.bakery.report.BakeryUpdateReportRepository;
+import com.depromeet.breadmapbackend.domain.bakery.view.BakeryViewRepository;
+import com.depromeet.breadmapbackend.domain.flag.FlagBakeryRepository;
+import com.depromeet.breadmapbackend.domain.flag.FlagRepository;
+import com.depromeet.breadmapbackend.domain.flag.FlagService;
+import com.depromeet.breadmapbackend.domain.notice.NoticeRepository;
+import com.depromeet.breadmapbackend.domain.notice.token.NoticeTokenRepository;
+import com.depromeet.breadmapbackend.domain.review.ReviewImageRepository;
+import com.depromeet.breadmapbackend.domain.review.ReviewProductRatingRepository;
+import com.depromeet.breadmapbackend.domain.review.ReviewRepository;
+import com.depromeet.breadmapbackend.domain.review.ReviewService;
+import com.depromeet.breadmapbackend.domain.review.comment.ReviewCommentRepository;
+import com.depromeet.breadmapbackend.domain.review.comment.like.ReviewCommentLikeRepository;
+import com.depromeet.breadmapbackend.domain.review.like.ReviewLikeRepository;
+import com.depromeet.breadmapbackend.domain.review.report.ReviewReportRepository;
+import com.depromeet.breadmapbackend.domain.review.view.ReviewViewRepository;
+import com.depromeet.breadmapbackend.domain.user.UserRepository;
+import com.depromeet.breadmapbackend.domain.user.UserService;
+import com.depromeet.breadmapbackend.domain.user.block.BlockUserRepository;
+import com.depromeet.breadmapbackend.domain.user.follow.FollowRepository;
+import com.depromeet.breadmapbackend.global.S3Uploader;
+import com.depromeet.breadmapbackend.global.infra.EmbeddedRedisConfig;
+import com.depromeet.breadmapbackend.global.infra.properties.CustomAWSS3Properties;
+import com.depromeet.breadmapbackend.global.infra.properties.CustomRedisProperties;
+import com.depromeet.breadmapbackend.global.security.token.JwtTokenProvider;
+import com.depromeet.breadmapbackend.global.security.token.RedisTokenUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest
 @AutoConfigureRestDocs
 @AutoConfigureMockMvc
 @Import({AmazonS3MockConfig.class, EmbeddedRedisConfig.class})
 public abstract class ControllerTest {
-    @Autowired
-    protected MockMvc mockMvc;
+	@Autowired
+	protected MockMvc mockMvc;
 
-    @Autowired
-    protected ObjectMapper objectMapper;
+	@Autowired
+	protected ObjectMapper objectMapper;
 
-    @Autowired
-    protected PasswordEncoder passwordEncoder;
+	@Autowired
+	protected PasswordEncoder passwordEncoder;
 
-    @MockBean
-    protected AuthService authService;
+	@MockBean
+	protected AuthService authService;
 
-    @Autowired
-    protected JwtTokenProvider jwtTokenProvider;
+	@Autowired
+	protected JwtTokenProvider jwtTokenProvider;
 
-    @Autowired
-    protected StringRedisTemplate redisTemplate;
+	@Autowired
+	protected StringRedisTemplate redisTemplate;
 
-    @Autowired
-    protected RedisTokenUtils redisTokenUtils;
+	@Autowired
+	protected RedisTokenUtils redisTokenUtils;
 
-    @Autowired
-    protected CustomRedisProperties customRedisProperties;
+	@Autowired
+	protected CustomRedisProperties customRedisProperties;
 
-    @Autowired
-    protected UserRepository userRepository;
+	@Autowired
+	protected UserRepository userRepository;
 
-    @Autowired
-    protected BakeryRepository bakeryRepository;
+	@Autowired
+	protected BakeryRepository bakeryRepository;
 
-    @Autowired
-    protected BakeryViewRepository bakeryViewRepository;
+	@Autowired
+	protected BakeryViewRepository bakeryViewRepository;
 
-    @Autowired
-    protected ProductRepository productRepository;
+	@Autowired
+	protected ProductRepository productRepository;
 
-    @Autowired
-    protected ReviewRepository reviewRepository;
+	@Autowired
+	protected ReviewRepository reviewRepository;
 
-    @Autowired
-    protected ReviewViewRepository reviewViewRepository;
+	@Autowired
+	protected ReviewViewRepository reviewViewRepository;
 
-    @Autowired
-    protected ReviewImageRepository reviewImageRepository;
+	@Autowired
+	protected ReviewImageRepository reviewImageRepository;
 
-    @Autowired
-    protected ReviewProductRatingRepository reviewProductRatingRepository;
+	@Autowired
+	protected ReviewProductRatingRepository reviewProductRatingRepository;
 
-    @Autowired
-    protected ReviewLikeRepository reviewLikeRepository;
+	@Autowired
+	protected ReviewLikeRepository reviewLikeRepository;
 
-    @Autowired
-    protected ReviewCommentRepository reviewCommentRepository;
+	@Autowired
+	protected ReviewCommentRepository reviewCommentRepository;
 
-    @Autowired
-    protected ReviewCommentLikeRepository reviewCommentLikeRepository;
+	@Autowired
+	protected ReviewCommentLikeRepository reviewCommentLikeRepository;
 
-    @Autowired
-    protected FlagRepository flagRepository;
+	@Autowired
+	protected FlagRepository flagRepository;
 
-    @Autowired
-    protected FlagBakeryRepository flagBakeryRepository;
+	@Autowired
+	protected FlagBakeryRepository flagBakeryRepository;
 
-    @Autowired
-    protected FollowRepository followRepository;
+	@Autowired
+	protected FollowRepository followRepository;
 
-    @Autowired
-    protected BakeryUpdateReportRepository bakeryUpdateReportRepository;
+	@Autowired
+	protected BakeryUpdateReportRepository bakeryUpdateReportRepository;
 
-    @Autowired
-    protected BakeryUpdateReportImageRepository bakeryUpdateReportImageRepository;
+	@Autowired
+	protected BakeryUpdateReportImageRepository bakeryUpdateReportImageRepository;
 
-    @Autowired
-    protected BakeryAddReportRepository bakeryAddReportRepository;
-    @Autowired
-    protected BakeryAddReportImageRepository bakeryAddReportImageRepository;
+	@Autowired
+	protected BakeryAddReportRepository bakeryAddReportRepository;
+	@Autowired
+	protected BakeryAddReportImageRepository bakeryAddReportImageRepository;
 
-    @Autowired
-    protected BakeryReportImageRepository bakeryReportImageRepository;
+	@Autowired
+	protected BakeryReportImageRepository bakeryReportImageRepository;
 
-    @Autowired
-    protected ProductAddReportRepository productAddReportRepository;
+	@Autowired
+	protected ProductAddReportRepository productAddReportRepository;
 
-    @Autowired
-    protected ProductAddReportImageRepository productAddReportImageRepository;
+	@Autowired
+	protected ProductAddReportImageRepository productAddReportImageRepository;
 
-    @Autowired
-    protected ReviewReportRepository reviewReportRepository;
+	@Autowired
+	protected ReviewReportRepository reviewReportRepository;
 
-    @Autowired
-    protected BlockUserRepository blockUserRepository;
+	@Autowired
+	protected BlockUserRepository blockUserRepository;
 
-    @Autowired
-    protected NoticeRepository noticeRepository;
+	@Autowired
+	protected NoticeRepository noticeRepository;
 
-    @Autowired
-    protected NoticeTokenRepository noticeTokenRepository;
+	@Autowired
+	protected NoticeTokenRepository noticeTokenRepository;
 
-    @Autowired
-    protected AdminRepository adminRepository;
+	@Autowired
+	protected AdminRepository adminRepository;
 
-    @Autowired
-    protected UserService userService;
+	@Autowired
+	protected UserService userService;
 
-    @Autowired
-    protected FlagService flagService;
+	@Autowired
+	protected FlagService flagService;
 
-    @Autowired
-    protected BakeryService bakeryService;
+	@Autowired
+	protected BakeryService bakeryService;
 
-    @Autowired
-    protected ReviewService reviewService;
+	@Autowired
+	protected ReviewService reviewService;
 
-    @Autowired
-    protected AdminService adminService;
+	@Autowired
+	protected AdminService adminService;
 
-    @Autowired
-    protected S3Uploader s3Uploader;
+	@Autowired
+	protected S3Uploader s3Uploader;
 
-    @Autowired
-    protected CustomAWSS3Properties customAWSS3Properties;
+	@Autowired
+	protected CustomAWSS3Properties customAWSS3Properties;
+
+	@AfterEach
+	void afterEach() {
+		redisTemplate.getConnectionFactory().getConnection().flushAll();
+	}
+
 }
