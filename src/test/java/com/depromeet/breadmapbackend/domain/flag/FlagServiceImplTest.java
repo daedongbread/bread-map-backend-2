c1cfb514fa8bd8e435e4f3ac2646aaf074eb326c
jaypark <pjhyun1990@gmail.com>
2023-06-12 13:33:05 +0900

    refactor : FlagServiceImpl 테스트 환경 세팅

---
+++ src/test/java/com/depromeet/breadmapbackend/domain/flag/FlagServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.depromeet.breadmapbackend.domain.flag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.depromeet.breadmapbackend.domain.notice.NoticeServiceTest;
+
+class FlagServiceImplTest extends NoticeServiceTest {
+
+	@Autowired
+	private FlagServiceImpl sut;
+
+	@Test
+	void User_Flag삭제_성공_테스트() throws Exception {
+		// given
+		// when
+		// then
+	}
+
+}
---
+++ src/test/java/com/depromeet/breadmapbackend/domain/flag/FlagServiceTest.java
@@ -0,0 +1,19 @@
+package com.depromeet.breadmapbackend.domain.flag;
+
+import org.springframework.context.annotation.Import;
+
+import com.depromeet.breadmapbackend.domain.notice.FcmService;
+import com.depromeet.breadmapbackend.domain.review.ReviewServiceImpl;
+import com.depromeet.breadmapbackend.global.infra.AsyncConfig;
+import com.depromeet.breadmapbackend.utils.ServiceTest;
+import com.depromeet.breadmapbackend.utils.TestConfig;
+
+@Import({
+	FlagServiceImpl.class,
+	FcmService.class,
+	TestConfig.class,
+	AsyncConfig.class,
+	ReviewServiceImpl.class,
+})
+public abstract class FlagServiceTest extends ServiceTest {
+}
