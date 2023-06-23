36dc062fe11ee02ac23727f0bc3c5b1601c6197a
jaypark <pjhyun1990@gmail.com>
2023-06-23 16:43:20 +0900

    refactor : BakeryController::getBakeryList 리팩토링 진행중.

--- src/main/java/com/depromeet/breadmapbackend/domain/bakery/BakeryQueryRepository.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/bakery/BakeryQueryRepository.java
@@ -1,5 +1,19 @@
 package com.depromeet.breadmapbackend.domain.bakery;
 
+import static com.depromeet.breadmapbackend.domain.bakery.QBakery.*;
+import static com.depromeet.breadmapbackend.domain.bakery.product.report.QProductAddReport.*;
+import static com.depromeet.breadmapbackend.domain.bakery.report.QBakeryReportImage.*;
+import static com.depromeet.breadmapbackend.domain.bakery.report.QBakeryUpdateReport.*;
+import static com.depromeet.breadmapbackend.domain.review.QReview.*;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
 import com.depromeet.breadmapbackend.domain.admin.bakery.param.AdminBakeryFilter;
 import com.depromeet.breadmapbackend.global.exception.DaedongException;
 import com.depromeet.breadmapbackend.global.exception.DaedongStatus;
@@ -7,86 +21,84 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-import static com.depromeet.breadmapbackend.domain.bakery.QBakery.bakery;
-import static com.depromeet.breadmapbackend.domain.bakery.product.report.QProductAddReport.productAddReport;
-import static com.depromeet.breadmapbackend.domain.bakery.product.report.QProductAddReportImage.productAddReportImage;
-import static com.depromeet.breadmapbackend.domain.bakery.report.QBakeryReportImage.bakeryReportImage;
-import static com.depromeet.breadmapbackend.domain.bakery.report.QBakeryUpdateReport.bakeryUpdateReport;
-import static com.depromeet.breadmapbackend.domain.review.QReview.review;
 
 @Repository
 @RequiredArgsConstructor
 public class BakeryQueryRepository {
-    private final JPAQueryFactory queryFactory;
-    private final int BAKERY_SIZE = 20;
+	private final JPAQueryFactory queryFactory;
+	private final int BAKERY_SIZE = 20;
 
-    public Page<Bakery> getAdminBakeryList(List<AdminBakeryFilter> filterBy, String name, int page) {
-        Pageable pageable = PageRequest.of(page, 20);
+	public List<Bakery> findTop20BakeriesByCoordinateRange(final CoordinateRange coordinateRange) {
+		return queryFactory
+			.select(bakery)
+			.from(bakery)
+			.where(bakery.latitude.between(coordinateRange.leftLatitude(), coordinateRange.rightLatitude())
+				.and(bakery.longitude.between(coordinateRange.downLongitude(), coordinateRange.upLongitude())))
+			.limit(BAKERY_SIZE)
+			.fetch();
+	}
 
-        List<Bakery> content = queryFactory.selectFrom(bakery)
-                .where(adminBakeryCondition(filterBy), searchCondition(name))
-                .offset((long) page * BAKERY_SIZE)
-                .limit(BAKERY_SIZE)
-                .fetch();
+	public Page<Bakery> getAdminBakeryList(List<AdminBakeryFilter> filterBy, String name, int page) {
+		Pageable pageable = PageRequest.of(page, 20);
 
-        Long count = queryFactory.select(bakery.count()).from(bakery)
-                .where(adminBakeryCondition(filterBy), searchCondition(name))
-                .fetchOne();
+		List<Bakery> content = queryFactory.selectFrom(bakery)
+			.where(adminBakeryCondition(filterBy), searchCondition(name))
+			.offset((long)page * BAKERY_SIZE)
+			.limit(BAKERY_SIZE)
+			.fetch();
 
-        return new PageImpl<>(content, pageable, count);
-    }
+		Long count = queryFactory.select(bakery.count()).from(bakery)
+			.where(adminBakeryCondition(filterBy), searchCondition(name))
+			.fetchOne();
 
-    private BooleanBuilder adminBakeryCondition(List<AdminBakeryFilter> filterBy) {
-        BooleanBuilder conditions = new BooleanBuilder();
-        if (filterBy != null) {
-            for (AdminBakeryFilter filter : filterBy) {
-                if (filter.equals(AdminBakeryFilter.BAKERY_REPORT_IMAGE)) {
-                    conditions.or(bakery.id.in(
-                            JPAExpressions.select(bakeryReportImage.bakery.id)
-                                    .from(bakeryReportImage)
-                                    .where(bakeryReportImage.isNew.isTrue())
-                                    .groupBy(bakeryReportImage.bakery.id)
-                                    .having(bakeryReportImage.count().gt(0L))));
-                } else if (filter.equals(AdminBakeryFilter.PRODUCT_ADD_REPORT)) {
-                    conditions.or(bakery.id.in(
-                            JPAExpressions.select(productAddReport.bakery.id)
-                                    .from(productAddReport)
-                                    .where(productAddReport.isNew.isTrue())
-                                    .groupBy(productAddReport.bakery.id)
-                                    .having(productAddReport.count().gt(0L))));
-                } else if (filter.equals(AdminBakeryFilter.BAKERY_UPDATE_REPORT)) {
-                    conditions.or(bakery.id.in(
-                            JPAExpressions.select(bakeryUpdateReport.bakery.id)
-                                    .from(bakeryUpdateReport)
-                                    .where(bakeryUpdateReport.isNew.isTrue())
-                                    .groupBy(bakeryUpdateReport.bakery.id)
-                                    .having(bakeryUpdateReport.count().gt(0L))));
-                } else if (filter.equals(AdminBakeryFilter.NEW_REVIEW)) {
-                    conditions.or(bakery.id.in(
-                            JPAExpressions.select(review.bakery.id)
-                                    .from(review)
-                                    .where(review.isNew.isTrue())
-                                    .groupBy(review.bakery.id)
-                                    .having(review.count().gt(0L))));
-                } else throw new DaedongException(DaedongStatus.ADMIN_FILTER_EXCEPTION);
-            }
-        }
-        return conditions;
-    }
+		return new PageImpl<>(content, pageable, count);
+	}
 
-    private BooleanExpression searchCondition(String name) {
-        if (name != null) {
-            return bakery.name.contains(name);
-        }
-        else return null;
-    }
+	private BooleanBuilder adminBakeryCondition(List<AdminBakeryFilter> filterBy) {
+		BooleanBuilder conditions = new BooleanBuilder();
+		if (filterBy != null) {
+			for (AdminBakeryFilter filter : filterBy) {
+				if (filter.equals(AdminBakeryFilter.BAKERY_REPORT_IMAGE)) {
+					conditions.or(bakery.id.in(
+						JPAExpressions.select(bakeryReportImage.bakery.id)
+							.from(bakeryReportImage)
+							.where(bakeryReportImage.isNew.isTrue())
+							.groupBy(bakeryReportImage.bakery.id)
+							.having(bakeryReportImage.count().gt(0L))));
+				} else if (filter.equals(AdminBakeryFilter.PRODUCT_ADD_REPORT)) {
+					conditions.or(bakery.id.in(
+						JPAExpressions.select(productAddReport.bakery.id)
+							.from(productAddReport)
+							.where(productAddReport.isNew.isTrue())
+							.groupBy(productAddReport.bakery.id)
+							.having(productAddReport.count().gt(0L))));
+				} else if (filter.equals(AdminBakeryFilter.BAKERY_UPDATE_REPORT)) {
+					conditions.or(bakery.id.in(
+						JPAExpressions.select(bakeryUpdateReport.bakery.id)
+							.from(bakeryUpdateReport)
+							.where(bakeryUpdateReport.isNew.isTrue())
+							.groupBy(bakeryUpdateReport.bakery.id)
+							.having(bakeryUpdateReport.count().gt(0L))));
+				} else if (filter.equals(AdminBakeryFilter.NEW_REVIEW)) {
+					conditions.or(bakery.id.in(
+						JPAExpressions.select(review.bakery.id)
+							.from(review)
+							.where(review.isNew.isTrue())
+							.groupBy(review.bakery.id)
+							.having(review.count().gt(0L))));
+				} else
+					throw new DaedongException(DaedongStatus.ADMIN_FILTER_EXCEPTION);
+			}
+		}
+		return conditions;
+	}
+
+	private BooleanExpression searchCondition(String name) {
+		if (name != null) {
+			return bakery.name.contains(name);
+		} else
+			return null;
+	}
 }
--- src/main/java/com/depromeet/breadmapbackend/domain/bakery/BakeryServiceImpl.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/bakery/BakeryServiceImpl.java
@@ -1,119 +1,140 @@
 package com.depromeet.breadmapbackend.domain.bakery;
 
-import com.depromeet.breadmapbackend.domain.bakery.view.BakeryView;
-import com.depromeet.breadmapbackend.domain.bakery.view.BakeryViewRepository;
-import com.depromeet.breadmapbackend.domain.review.ReviewService;
-import com.depromeet.breadmapbackend.domain.user.block.BlockUserRepository;
-import com.depromeet.breadmapbackend.global.exception.DaedongException;
-import com.depromeet.breadmapbackend.global.exception.DaedongStatus;
-import com.depromeet.breadmapbackend.domain.flag.FlagColor;
-import com.depromeet.breadmapbackend.domain.flag.FlagBakeryRepository;
-import com.depromeet.breadmapbackend.domain.review.ReviewProductRating;
-import com.depromeet.breadmapbackend.domain.review.Review;
-import com.depromeet.breadmapbackend.domain.user.User;
-import com.depromeet.breadmapbackend.domain.user.UserRepository;
-import com.depromeet.breadmapbackend.domain.bakery.dto.*;
-import com.depromeet.breadmapbackend.domain.review.dto.MapSimpleReviewDto;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import com.depromeet.breadmapbackend.domain.bakery.dto.BakeryCardDto;
+import com.depromeet.breadmapbackend.domain.bakery.dto.BakeryDto;
+import com.depromeet.breadmapbackend.domain.bakery.view.BakeryView;
+import com.depromeet.breadmapbackend.domain.bakery.view.BakeryViewRepository;
+import com.depromeet.breadmapbackend.domain.flag.FlagBakeryRepository;
+import com.depromeet.breadmapbackend.domain.flag.FlagColor;
+import com.depromeet.breadmapbackend.domain.review.Review;
+import com.depromeet.breadmapbackend.domain.review.ReviewService;
+import com.depromeet.breadmapbackend.domain.review.dto.MapSimpleReviewDto;
+import com.depromeet.breadmapbackend.domain.user.User;
+import com.depromeet.breadmapbackend.domain.user.UserRepository;
+import com.depromeet.breadmapbackend.global.exception.DaedongException;
+import com.depromeet.breadmapbackend.global.exception.DaedongStatus;
 
-import static java.lang.Math.*;
-import static java.lang.Math.toRadians;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class BakeryServiceImpl implements BakeryService {
-    private final BakeryRepository bakeryRepository;
-    private final BakeryViewRepository bakeryViewRepository;
-    private final UserRepository userRepository;
-    private final BlockUserRepository blockUserRepository;
-    private final FlagBakeryRepository flagBakeryRepository;
-    private final ReviewService reviewService;
+	private final BakeryRepository bakeryRepository;
+	private final BakeryQueryRepository bakeryQueryRepository;
+	private final BakeryViewRepository bakeryViewRepository;
+	private final UserRepository userRepository;
+	private final FlagBakeryRepository flagBakeryRepository;
+	private final ReviewService reviewService;
 
-    @Transactional(readOnly = true, rollbackFor = Exception.class)
-    public List<BakeryCardDto> getBakeryList(
-            String oAuthId, BakerySortType sortBy, boolean filterBy,
-            Double latitude, Double longitude, Double latitudeDelta, Double longitudeDelta) {
+	@Transactional(readOnly = true, rollbackFor = Exception.class)
+	public List<BakeryCardDto> getBakeryList(
+		final String oAuthId,
+		final BakerySortType sortBy,
+		final boolean filterBy,
+		final Double latitude,
+		final Double longitude,
+		final Double latitudeDelta,
+		final Double longitudeDelta
+	) {
 
-        Comparator<BakeryCardDto> comparing;
-        if(sortBy.equals(BakerySortType.DISTANCE)) comparing = Comparator.comparing(BakeryCardDto::getDistance);
-        else if(sortBy.equals(BakerySortType.POPULAR)) comparing = Comparator.comparing(BakeryCardDto::getPopularNum).reversed();
-        else throw new DaedongException(DaedongStatus.BAKERY_SORT_TYPE_EXCEPTION);
-        User me = userRepository.findByOAuthId(oAuthId).orElseThrow(() -> new DaedongException(DaedongStatus.USER_NOT_FOUND));
-        List<Bakery> bakeries = bakeryRepository
-                .findTop20ByLatitudeBetweenAndLongitudeBetweenAndStatus(
-                        latitude - latitudeDelta / 2, latitude + latitudeDelta / 2,
-                        longitude - longitudeDelta / 2, longitude + longitudeDelta / 2,
-                        BakeryStatus.POSTING);
+		User me = userRepository.findByOAuthId(oAuthId)
+			.orElseThrow(() -> new DaedongException(DaedongStatus.USER_NOT_FOUND));
 
-        return bakeries.stream()
-                .map(bakery -> {
-                    List<Review> reviewList = reviewService.getReviewList(me, bakery);
-//                    List<Review> reviewList = bakery.getReviewList().stream()
-//                            .filter(Review::isValid)
-//                            .filter(review -> blockUserRepository.findByFromUserAndToUser(me, review.getUser()).isEmpty())
-//                            .collect(Collectors.toList());
-                    FlagColor color = null;
-                    if (!filterBy) color = FlagColor.ORANGE;
-                    else color = flagBakeryRepository.findFlagByBakeryAndUser(bakery, me).isPresent() ?
-                            flagBakeryRepository.findFlagByBakeryAndUser(bakery, me).get().getColor() : FlagColor.GRAY;
-                    return BakeryCardDto.builder()
-                            .bakery(bakery)
-                            .flagNum(flagBakeryRepository.countFlagNum(bakery))
-                            .rating(bakeryRating(reviewList))
-                            .reviewNum(reviewList.size())
-                            .simpleReviewList(reviewList.stream()
-                                    .sorted(Comparator.comparing(Review::getCreatedAt).reversed()).map(MapSimpleReviewDto::new)
-                                    .limit(3).collect(Collectors.toList()))
-                            .distance(floor(acos(cos(toRadians(latitude))
-                                    * cos(toRadians(bakery.getLatitude()))
-                                    * cos(toRadians(bakery.getLongitude())- toRadians(longitude))
-                                    + sin(toRadians(latitude))*sin(toRadians(bakery.getLatitude())))*6371000))
-                            .color(color)
-                            .build();
-                })
-                .sorted(comparing)
-                .collect(Collectors.toList());
-    }
+		List<Bakery> bakeries = bakeryQueryRepository
+			.findTop20BakeriesByCoordinateRange(
+				CoordinateRange.of(latitude, latitudeDelta, longitude, longitudeDelta)
+			);
 
-    @Transactional(rollbackFor = Exception.class)
-    public BakeryDto getBakery(String oAuthId, Long bakeryId) {
-        User me = userRepository.findByOAuthId(oAuthId).orElseThrow(() -> new DaedongException(DaedongStatus.USER_NOT_FOUND));
-        Bakery bakery = bakeryRepository.findByIdAndStatus(bakeryId, BakeryStatus.POSTING).orElseThrow(() -> new DaedongException(DaedongStatus.BAKERY_NOT_FOUND));
-        bakeryViewRepository.findByBakery(bakery)
-                .orElseGet(() -> {
-                    BakeryView bakeryView = BakeryView.builder().bakery(bakery).build();
-                    return bakeryViewRepository.save(bakeryView);
-                }).viewBakery();
+		return bakeries.stream()
+			.map(bakery -> {
+				List<Review> reviewList = reviewService.getReviewList(me, bakery);
+				FlagColor color = null;
+				if (!filterBy)
+					color = FlagColor.ORANGE;
+				else
+					color = flagBakeryRepository.findFlagByBakeryAndUser(bakery, me).isPresent() ?
+						flagBakeryRepository.findFlagByBakeryAndUser(bakery, me).get().getColor() : FlagColor.GRAY;
+				return BakeryCardDto.builder()
+					.bakery(bakery)
+					.flagNum(flagBakeryRepository.countFlagNum(bakery))
+					.rating(bakeryRating(reviewList))
+					.reviewNum(reviewList.size())
+					.simpleReviewList(getSimpleReviewListFrom(reviewList))
+					.distance(bakery.getDistanceFromUser(latitude, longitude))
+					.color(color)
+					.build();
+			})
+			.sorted(getBakeryComparator(sortBy))
+			.collect(Collectors.toList());
+	}
 
-//        List<Review> reviewList = bakery.getReviewList().stream()
-//                .filter(Review::isValid)
-//                .filter(review -> blockUserRepository.findByFromUserAndToUser(me, review.getUser()).isEmpty())
-//                .collect(Collectors.toList());
-        List<Review> reviewList = reviewService.getReviewList(me, bakery);
+	@Transactional(rollbackFor = Exception.class)
+	public BakeryDto getBakery(String oAuthId, Long bakeryId) {
+		User me = userRepository.findByOAuthId(oAuthId)
+			.orElseThrow(() -> new DaedongException(DaedongStatus.USER_NOT_FOUND));
+		Bakery bakery = bakeryRepository.findByIdAndStatus(bakeryId, BakeryStatus.POSTING)
+			.orElseThrow(() -> new DaedongException(DaedongStatus.BAKERY_NOT_FOUND));
+		bakeryViewRepository.findByBakery(bakery)
+			.orElseGet(() -> {
+				BakeryView bakeryView = BakeryView.builder().bakery(bakery).build();
+				return bakeryViewRepository.save(bakeryView);
+			}).viewBakery();
 
-        BakeryDto.BakeryInfo bakeryInfo = BakeryDto.BakeryInfo.builder()
-                .bakery(bakery)
-                .flagNum(flagBakeryRepository.countFlagNum(bakery))
-                .rating(bakeryRating(reviewList))
-                .reviewNum(reviewList.size())
-                .build();
-        BakeryDto.FlagInfo flagInfo = BakeryDto.FlagInfo.builder()
-                .flagBakery(flagBakeryRepository.findByBakeryAndUser(bakery, me).orElse(null)).build();
-        BakeryDto.PioneerInfo pioneerInfo = BakeryDto.PioneerInfo.builder().pioneer(bakery.getPioneer()).build();
+		//        List<Review> reviewList = bakery.getReviewList().stream()
+		//                .filter(Review::isValid)
+		//                .filter(review -> blockUserRepository.findByFromUserAndToUser(me, review.getUser()).isEmpty())
+		//                .collect(Collectors.toList());
+		List<Review> reviewList = reviewService.getReviewList(me, bakery);
 
-        return BakeryDto.builder()
-                .bakeryInfo(bakeryInfo).flagInfo(flagInfo).facilityInfoList(bakery.getFacilityInfoList()).pioneerInfo(pioneerInfo).build();
-    }
+		BakeryDto.BakeryInfo bakeryInfo = BakeryDto.BakeryInfo.builder()
+			.bakery(bakery)
+			.flagNum(flagBakeryRepository.countFlagNum(bakery))
+			.rating(bakeryRating(reviewList))
+			.reviewNum(reviewList.size())
+			.build();
+		BakeryDto.FlagInfo flagInfo = BakeryDto.FlagInfo.builder()
+			.flagBakery(flagBakeryRepository.findByBakeryAndUser(bakery, me).orElse(null)).build();
+		BakeryDto.PioneerInfo pioneerInfo = BakeryDto.PioneerInfo.builder().pioneer(bakery.getPioneer()).build();
 
-    private Double bakeryRating(List<Review> reviewList) {
-        return Math.floor(reviewList.stream().map(Review::getAverageRating).collect(Collectors.toList())
-                .stream().mapToDouble(Double::doubleValue).average().orElse(0)*10)/10.0;
-    }
+		return BakeryDto.builder()
+			.bakeryInfo(bakeryInfo)
+			.flagInfo(flagInfo)
+			.facilityInfoList(bakery.getFacilityInfoList())
+			.pioneerInfo(pioneerInfo)
+			.build();
+	}
+
+	private List<MapSimpleReviewDto> getSimpleReviewListFrom(final List<Review> reviewList) {
+		return reviewList.stream()
+			.sorted(Comparator.comparing(Review::getCreatedAt).reversed())
+			.map(MapSimpleReviewDto::new)
+			.limit(3)
+			.collect(Collectors.toList());
+	}
+
+	private Comparator<BakeryCardDto> getBakeryComparator(final BakerySortType sortBy) {
+		Comparator<BakeryCardDto> comparing;
+
+		if (sortBy.equals(BakerySortType.DISTANCE))
+			comparing = Comparator.comparing(BakeryCardDto::getDistance);
+		else if (sortBy.equals(BakerySortType.POPULAR))
+			comparing = Comparator.comparing(BakeryCardDto::getPopularNum).reversed();
+		else
+			throw new DaedongException(DaedongStatus.BAKERY_SORT_TYPE_EXCEPTION);
+		return comparing;
+	}
+
+	private Double bakeryRating(List<Review> reviewList) {
+		return Math.floor(reviewList.stream().map(Review::getAverageRating).collect(Collectors.toList())
+			.stream().mapToDouble(Double::doubleValue).average().orElse(0) * 10) / 10.0;
+	}
 }
