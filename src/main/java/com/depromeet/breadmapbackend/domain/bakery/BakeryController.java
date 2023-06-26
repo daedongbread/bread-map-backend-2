2efdd1529167af5202c8df4dca939dbf30de3346
jaypark <pjhyun1990@gmail.com>
2023-06-26 14:37:59 +0900

    refactor : BakeryController::getBakeryList 리팩토링

--- src/main/java/com/depromeet/breadmapbackend/domain/bakery/BakeryController.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/bakery/BakeryController.java
@@ -2,21 +2,20 @@ package com.depromeet.breadmapbackend.domain.bakery;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.depromeet.breadmapbackend.domain.bakery.dto.BakeryCardDto;
 import com.depromeet.breadmapbackend.domain.bakery.dto.BakeryDto;
-import com.depromeet.breadmapbackend.global.annotation.EnumCheck;
 import com.depromeet.breadmapbackend.global.dto.ApiResponse;
-import com.depromeet.breadmapbackend.global.exception.ValidationGroups;
 import com.depromeet.breadmapbackend.global.exception.ValidationSequence;
 import com.depromeet.breadmapbackend.global.security.CurrentUser;
 import com.depromeet.breadmapbackend.global.security.userinfo.CurrentUserInfo;
@@ -34,19 +33,17 @@ public class BakeryController {
 	@ResponseStatus(HttpStatus.OK)
 	public ApiResponse<List<BakeryCardDto>> getBakeryList(
 		@AuthenticationPrincipal CurrentUserInfo currentUserInfo,
-		@RequestParam @EnumCheck(groups = ValidationGroups.PatternCheckGroup.class) BakerySortType sortBy,
-		@RequestParam Boolean filterBy,
-		@RequestParam Double latitude, @RequestParam Double longitude,
-		@RequestParam Double latitudeDelta, @RequestParam Double longitudeDelta) {
+		@Valid final BakeryListRequest bakeryListRequest
+	) {
 		return new ApiResponse<>(
 			bakeryService.getBakeryList(
 				currentUserInfo.getId(),
-				sortBy,
-				filterBy,
-				latitude,
-				longitude,
-				latitudeDelta,
-				longitudeDelta
+				bakeryListRequest.sortBy(),
+				bakeryListRequest.filterBy(),
+				bakeryListRequest.latitude(),
+				bakeryListRequest.longitude(),
+				bakeryListRequest.latitudeDelta(),
+				bakeryListRequest.longitudeDelta()
 			)
 		);
 	}
