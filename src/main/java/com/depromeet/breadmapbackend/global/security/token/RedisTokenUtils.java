ba31666f63fad7a6c699cb2db4d754f57b6e78fd
jaypark <pjhyun1990@gmail.com>
2023-07-07 17:00:48 +0900

    fix : 2023-07-06 18:01:53 오류 수정

--- src/main/java/com/depromeet/breadmapbackend/global/security/token/RedisTokenUtils.java
+++ src/main/java/com/depromeet/breadmapbackend/global/security/token/RedisTokenUtils.java
@@ -1,55 +1,62 @@
 package com.depromeet.breadmapbackend.global.security.token;
 
-import com.depromeet.breadmapbackend.global.infra.properties.CustomRedisProperties;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
+import com.depromeet.breadmapbackend.global.infra.properties.CustomRedisProperties;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class RedisTokenUtils {
-    private final StringRedisTemplate redisTemplate;
-    private final CustomRedisProperties customRedisProperties;
-    private static final String INVALID = "invalid";
+	private final StringRedisTemplate redisTemplate;
+	private final CustomRedisProperties customRedisProperties;
+	private static final String INVALID = "invalid";
 
+	public void withDrawal(String oAuthId) {
+		redisTemplate.opsForValue().set(customRedisProperties.getKey().getDelete() + ":" + oAuthId, oAuthId);
+	}
 
-    public void withDrawal(String oAuthId) {
-        redisTemplate.opsForValue().set(customRedisProperties.getKey().getDelete() + ":" + oAuthId, oAuthId);
-    }
+	public Boolean isRejoinPossible(String oAuthId) {
+		return Boolean.FALSE.equals(redisTemplate.hasKey(customRedisProperties.getKey().getDelete() + ":" + oAuthId));
+	}
 
-    public Boolean isRejoinPossible(String oAuthId) {
-        return !redisTemplate.hasKey(customRedisProperties.getKey().getDelete() + ":" + oAuthId);
-    }
+	public void setRefreshToken(String refreshToken, String value, Long expiredDate) {
+		redisTemplate.opsForValue().set(refreshToken, value, expiredDate, TimeUnit.MILLISECONDS);
+	}
 
-    public void setRefreshToken(String refreshToken, String value, Long expiredDate) {
-        redisTemplate.opsForValue().set(refreshToken, value, expiredDate, TimeUnit.MILLISECONDS);
-    }
+	public Boolean isRefreshTokenValid(String refreshToken, String accessToken) {
+		final Optional<String> accessTokenWithOAuthId = getAccessTokenWithOAuthId(refreshToken);
 
-    public Boolean isRefreshTokenValid(String refreshToken, String accessToken) {
-        if (redisTemplate.opsForValue().get(refreshToken) != null)
-            return accessToken.equals(redisTemplate.opsForValue().get(refreshToken).split(":")[1]);
-        else return Boolean.FALSE;
-    }
+		return accessTokenWithOAuthId.isPresent() && accessToken.equals(accessTokenWithOAuthId.get().split(":")[1]);
+	}
 
-    public String getOAuthIdFromRefreshToken(String refreshToken) {
-        if (redisTemplate.opsForValue().get(refreshToken) != null)
-            return redisTemplate.opsForValue().get(refreshToken).split(":")[0];
-        else return "";
-    }
+	public String getOAuthIdFromRefreshToken(String refreshToken) {
+		final Optional<String> accessTokenWithOAuthId = getAccessTokenWithOAuthId(refreshToken);
 
-    public void setAccessTokenBlackList(String accessToken, Long expirationDate) {
-        redisTemplate.opsForValue().set(accessToken, INVALID, expirationDate, TimeUnit.MILLISECONDS);
-    }
+		return accessTokenWithOAuthId.isPresent() ? accessTokenWithOAuthId.get().split(":")[0] : "";
+	}
 
-    public void deleteRefreshToken(String refreshToken) {
-        redisTemplate.delete(refreshToken);
-    }
+	public void setAccessTokenBlackList(String accessToken, Long expirationDate) {
+		redisTemplate.opsForValue().set(accessToken, INVALID, expirationDate, TimeUnit.MILLISECONDS);
+	}
 
-    public Boolean isBlackList(String accessToken) {
-        return (INVALID.equals(redisTemplate.opsForValue().get(accessToken))) ? Boolean.TRUE : Boolean.FALSE;
-    }
+	public void deleteRefreshToken(String refreshToken) {
+		redisTemplate.delete(refreshToken);
+	}
+
+	public Boolean isBlackList(String accessToken) {
+		return (INVALID.equals(redisTemplate.opsForValue().get(accessToken))) ? Boolean.TRUE : Boolean.FALSE;
+	}
+
+	private Optional<String> getAccessTokenWithOAuthId(final String refreshToken) {
+		return Optional.ofNullable(redisTemplate.opsForValue().get(refreshToken));
+	}
+
 }
