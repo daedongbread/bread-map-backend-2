2037ae5ee3b544d60ab3d05b0a9e9a3117a39220
jaypark <pjhyun1990@gmail.com>
2023-07-10 23:35:30 +0900

    test : 동시성 테스트 추가.

--- src/test/java/com/depromeet/breadmapbackend/domain/bakery/BakeryServiceImplTest.java
+++ src/test/java/com/depromeet/breadmapbackend/domain/bakery/BakeryServiceImplTest.java
@@ -6,10 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
 import com.depromeet.breadmapbackend.domain.bakery.dto.BakeryDto;
@@ -34,21 +39,56 @@ class BakeryServiceImplTest {
 	@Autowired
 	private BakeryViewRepository repository;
 
+	@Autowired
+	private StringRedisTemplate redisTemplate;
+
+	private final Long userId = 111L;
+	private final Long bakeryId = 100L;
+
+	@BeforeEach
+	void setUp() {
+		redisTemplate.opsForValue().getAndDelete("BAKERY-VIEW:" + bakeryId + ":" + LocalDate.now());
+	}
 
 	@Test
 	@Sql("classpath:bakery-test-data.sql")
 	void 최초조회() throws Exception{
-	    //given
-		final Long userId = 111L;
-		final Long bakeryId = 100L;
-
+		//given
 	    //when
 		final BakeryDto result = sut.getBakery(userId, bakeryId);
 
 		//then
 		assertThat(result).isNotNull();
-		Thread.sleep(10000);
+		Thread.sleep(1000);
 		assertThat(repository.findById(new BakeryViewId(bakeryId, now())).get().getViewCount()).isEqualTo(1L);
+	}
+
+	@Test
+	@Sql("classpath:bakery-test-data.sql")
+	void 동시_조회() throws Exception {
+		//given
+
+		final int threadCount = 500;
+		final ExecutorService executorService = Executors.newFixedThreadPool(16);
+		final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+		//when
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					sut.getBakery(userId, bakeryId);
+				} finally {
+					countDownLatch.countDown();
+				}
+			});
+		}
+		countDownLatch.await();
+		Thread.sleep(1000);
+
+		assertThat(repository.findById(
+			new BakeryViewId(bakeryId, now())
+		).get().getViewCount()).isEqualTo(500L);
+
 
 	}
+
 }
