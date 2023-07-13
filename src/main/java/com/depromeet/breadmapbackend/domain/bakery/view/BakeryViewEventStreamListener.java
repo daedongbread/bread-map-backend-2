86da22503ea3cb16fc8673fb95bad5e85f0d8446
jaypark <pjhyun1990@gmail.com>
2023-07-13 23:05:58 +0900

    refactor : redis 데이터 유실 문제 처리

--- src/main/java/com/depromeet/breadmapbackend/domain/bakery/view/BakeryViewEventStreamListener.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/bakery/view/BakeryViewEventStreamListener.java
@@ -1,17 +1,19 @@
 package com.depromeet.breadmapbackend.domain.bakery.view;
 
+import static com.depromeet.breadmapbackend.global.EventConsumerGroupInfo.*;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.stream.StreamListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,23 +23,51 @@ public class BakeryViewEventStreamListener implements StreamListener<String, Map
 
 	private final BakeryViewRepository repository;
 	private final StringRedisTemplate redisTemplate;
-	public static final String KEY = "BAKERY-VIEW:";
+	private static final Long INITIAL_COUNT = 1L;
 
 	@Transactional
 	@Override
 	public void onMessage(final MapRecord<String, String, String> message) {
+		final List<String> keys = BAKERY_VIEW_COUNT.getEvenMessageKeys();
 		final Map<String, String> value = message.getValue();
-		final Long bakeryId = Long.parseLong(value.get("bakeryId"));
-		final LocalDate viewDate = LocalDate.parse(value.get("viewDate"), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-		final Long increment = redisTemplate.opsForValue()
-			.increment(KEY + bakeryId + ":" + viewDate);
+		final Long bakeryId = Long.parseLong(value.get(keys.get(0)));
+		final LocalDate viewDate = LocalDate.parse(value.get(keys.get(1)), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+		final Long viewCount = getIncrementedViewCount(bakeryId, viewDate);
 
 		repository.save(BakeryView.builder()
 			.bakeryId(bakeryId)
 			.viewDate(viewDate)
-			.viewCount(increment)
+			.viewCount(viewCount)
 			.build()
 		);
+	}
+
+	private Long getIncrementedViewCount(final Long bakeryId, final LocalDate viewDate) {
+		final Long cachedViewCount = redisTemplate.opsForValue()
+			.increment(getRedisViewCountKey(bakeryId, viewDate));
+
+		if (cachedViewCount == null || Objects.equals(cachedViewCount , INITIAL_COUNT)) {
+			return reCountWhenErrorOrInitialCount(bakeryId, viewDate);
+		}else {
+			return cachedViewCount;
+		}
+	}
+
+	private Long reCountWhenErrorOrInitialCount(final Long bakeryId, final LocalDate viewDate) {
+		final Optional<BakeryView> bakeryView = repository.findById(new BakeryViewId(bakeryId, viewDate));
+
+		if (bakeryView.isPresent()) {
+			final Long incrementedViewCount = (bakeryView.get().getViewCount()) + 1L;
+			redisTemplate.opsForValue().set(getRedisViewCountKey(bakeryId, viewDate), incrementedViewCount.toString());
+			return incrementedViewCount;
+		}else{
+			return INITIAL_COUNT;
+		}
+	}
+
+	private String getRedisViewCountKey(final Long bakeryId, final LocalDate viewDate) {
+		return "BAKERY-VIEW:" + bakeryId + ":" + viewDate;
 	}
 }
