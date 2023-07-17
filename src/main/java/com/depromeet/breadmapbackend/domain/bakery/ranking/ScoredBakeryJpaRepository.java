b3f66174163b7ad382108f7574876f8847343add
jaypark <pjhyun1990@gmail.com>
2023-07-18 01:57:41 +0900

    feat : ScoredBakery 정렬조건 변경

--- src/main/java/com/depromeet/breadmapbackend/domain/bakery/ranking/ScoredBakeryJpaRepository.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/bakery/ranking/ScoredBakeryJpaRepository.java
@@ -21,7 +21,7 @@ public interface ScoredBakeryJpaRepository extends JpaRepository<ScoredBakery, L
 		+ "from ScoredBakery sb "
 		+ "join fetch sb.bakery b "
 		+ "where sb.calculatedDate = :calculatedDate "
-		+ "order by sb.totalScore desc, sb.bakery.id desc")
+		+ "order by sb.rank desc ")
 	List<ScoredBakery> findScoredBakeryByCalculatedDate(
 		@Param("calculatedDate") final LocalDate calculatedDate,
 		final Pageable pageable
