5721f6f7b5e64f8ef966bd3557fc08669cb4a9ed
k-gn <rlarbska97@gmail.com>
2023-07-17 16:42:11 +0900

    feat: 커뮤니티 기본 구조 작업

--- src/main/java/com/depromeet/breadmapbackend/domain/review/post/PostService.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/review/post/PostService.java
@@ -1,18 +1,26 @@
 package com.depromeet.breadmapbackend.domain.review.post;
 
+import java.util.List;
+
+import com.depromeet.breadmapbackend.domain.review.post.dto.PostRequestDto;
+import com.depromeet.breadmapbackend.domain.review.post.dto.PostResponseDto;
+
 public interface PostService {
 
 	// post 등록
-	void register(String oAuthId);
+	void register(
+		PostRequestDto postRequestDto,
+		String oAuthId
+	);
 
 	// post 조회
-	Object getPosts();
+	List<PostResponseDto> getPosts();
 
 	// post 상세 조회
-	Object getPost(Long postId);
+	PostResponseDto getPost(Long postId);
 
 	// post 추천글 조회
-	Object getHotPosts();
+	List<PostResponseDto> getHotPosts();
 
 	// post 삭제
 	void remove(
--- src/main/java/com/depromeet/breadmapbackend/domain/review/post/PostServiceImpl.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/review/post/PostServiceImpl.java
@@ -1,9 +1,13 @@
 package com.depromeet.breadmapbackend.domain.review.post;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.depromeet.breadmapbackend.domain.review.ReviewRepository;
+import com.depromeet.breadmapbackend.domain.review.post.dto.PostRequestDto;
+import com.depromeet.breadmapbackend.domain.review.post.dto.PostResponseDto;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,22 +21,25 @@ public class PostServiceImpl implements PostService {
 	private final ReviewRepository reviewRepository;
 
 	@Override
-	public void register(String oAuthId) {
-
+	public void register(
+		PostRequestDto dto,
+		String oAuthId
+	) {
+		// TODO register
 	}
 
 	@Override
-	public Object getPosts() {
+	public List<PostResponseDto> getPosts() {
 		return null;
 	}
 
 	@Override
-	public Object getPost(Long postId) {
+	public PostResponseDto getPost(Long postId) {
 		return null;
 	}
 
 	@Override
-	public Object getHotPosts() {
+	public List<PostResponseDto> getHotPosts() {
 		return null;
 	}
 
