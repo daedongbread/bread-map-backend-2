58e0e3e026cfc02154fd0ba5bd4bbd8336b84909
jaypark <pjhyun1990@gmail.com>
2023-07-23 23:58:40 +0900

    test : controller test 작성

---
+++ src/test/java/com/depromeet/breadmapbackend/domain/post/comment/CommentControllerAssertions.java
@@ -0,0 +1,32 @@
+package com.depromeet.breadmapbackend.domain.post.comment;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.springframework.test.web.servlet.ResultActions;
+
+/**
+ * PostControllerAssertions
+ *
+ * @author jaypark
+ * @version 1.0.0
+ * @since 2023/07/23
+ */
+public class CommentControllerAssertions {
+
+	public static void 댓글_추가_요청_결과_검증(final ResultActions 결과) throws Exception {
+		결과.andExpect(status().isCreated());
+	}
+
+	public static void 커뮤니티_전체_카테고리_조회_요청_결과_검증(final ResultActions 결과) throws Exception {
+		결과.andExpect(status().isOk());
+	}
+
+	public static void 빵_이야기_댓글_조회_요청_결과_검증(final ResultActions 결과) throws Exception {
+		결과.andExpect(status().isOk());
+	}
+
+	public static void 댓글_삭제_요청_결과_검증(final ResultActions 결과) throws Exception {
+		결과.andExpect(status().isOk());
+	}
+
+}
