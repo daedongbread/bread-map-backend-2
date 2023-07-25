ffa6cc199ce29cc7f76e7f5e0aee30f88dae3052
jaypark <pjhyun1990@gmail.com>
2023-07-26 00:24:56 +0900

    redis stream 이벤트 생성 오류 수정

--- src/main/java/com/depromeet/breadmapbackend/domain/bakery/ranking/ScoredBakeryController.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/bakery/ranking/ScoredBakeryController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.depromeet.breadmapbackend.domain.bakery.ranking.dto.BakeryRankingCard;
 import com.depromeet.breadmapbackend.global.dto.ApiResponse;
+import com.depromeet.breadmapbackend.global.exception.DaedongException;
+import com.depromeet.breadmapbackend.global.exception.DaedongStatus;
 import com.depromeet.breadmapbackend.global.security.userinfo.CurrentUserInfo;
 
 import lombok.RequiredArgsConstructor;
@@ -37,8 +39,14 @@ public class ScoredBakeryController {
 		@AuthenticationPrincipal final CurrentUserInfo currentUserInfo,
 		@PathVariable("count") final int count
 	) {
+		final List<BakeryRankingCard> bakeriesRankTop =
+			scoredBakeryService.findBakeriesRankTop(currentUserInfo.getId(), count);
+
+		if (bakeriesRankTop.size() == 0)
+			throw new DaedongException(DaedongStatus.CALCULATING_BAKERY_RANKING);
+		
 		return new ApiResponse<>(
-			scoredBakeryService.findBakeriesRankTop(currentUserInfo.getId(), count)
+			bakeriesRankTop
 		);
 	}
 }
