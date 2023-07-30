63f652d015a6a7e38e2e8e464f930a871b022de7
skehdxhd96 <xman96@naver.com>
2023-07-30 23:33:41 +0900

    feat. 빵집 조회 api 도로명 주소 추가

--- src/main/java/com/depromeet/breadmapbackend/domain/admin/bakery/dto/AdminSimpleBakeryDto.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/admin/bakery/dto/AdminSimpleBakeryDto.java
@@ -1,45 +1,47 @@
 package com.depromeet.breadmapbackend.domain.admin.bakery.dto;
 
+import java.time.format.DateTimeFormatter;
 
 import com.depromeet.breadmapbackend.domain.bakery.Bakery;
-
-
 import com.depromeet.breadmapbackend.domain.bakery.BakeryStatus;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.format.DateTimeFormatter;
-
 @Getter
 @NoArgsConstructor
 public class AdminSimpleBakeryDto {
-    private Long bakeryId;
-    private String name;
-    private Integer bakeryReportImageNum;
-    private Integer productAddReportNum;
-    private Integer bakeryUpdateReportNum;
-    private Integer newReviewNum;
-    private String createdAt;
-    private String modifiedAt;
-    private BakeryStatus status;
+	private Long bakeryId;
+	private String name;
+	private String address;
+	private String detailedAddress;
+	private Integer bakeryReportImageNum;
+	private Integer productAddReportNum;
+	private Integer bakeryUpdateReportNum;
+	private Integer newReviewNum;
+	private String createdAt;
+	private String modifiedAt;
+	private BakeryStatus status;
 
-    @Builder
-    public AdminSimpleBakeryDto(
-            Bakery bakery,
-            Integer bakeryReportImageNum,
-            Integer productAddReportNum,
-            Integer bakeryUpdateReportNum,
-            Integer newReviewNum
-    ) {
-        this.bakeryId = bakery.getId();
-        this.name = bakery.getName();
-        this.bakeryReportImageNum = bakeryReportImageNum;
-        this.productAddReportNum = productAddReportNum;
-        this.bakeryUpdateReportNum = bakeryUpdateReportNum;
-        this.newReviewNum = newReviewNum;
-        this.createdAt = bakery.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        this.modifiedAt = bakery.getModifiedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        this.status = bakery.getStatus();
-    }
+	@Builder
+	public AdminSimpleBakeryDto(
+		Bakery bakery,
+		Integer bakeryReportImageNum,
+		Integer productAddReportNum,
+		Integer bakeryUpdateReportNum,
+		Integer newReviewNum
+	) {
+		this.bakeryId = bakery.getId();
+		this.name = bakery.getName();
+		this.address = bakery.getAddress();
+		this.detailedAddress = bakery.getDetailedAddress();
+		this.bakeryReportImageNum = bakeryReportImageNum;
+		this.productAddReportNum = productAddReportNum;
+		this.bakeryUpdateReportNum = bakeryUpdateReportNum;
+		this.newReviewNum = newReviewNum;
+		this.createdAt = bakery.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		this.modifiedAt = bakery.getModifiedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		this.status = bakery.getStatus();
+	}
 }
