bd420d54f266e8c7acc3e1268e6ab8ee7df095dd
Chris Park <dev.chris.kr@gmail.com>
2023-11-25 05:01:29 +0900

    feat: OpenSearch test scheduler profile 수정

--- src/main/java/com/depromeet/breadmapbackend/domain/search/OpenSearchLoadTestScheduler.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/search/OpenSearchLoadTestScheduler.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
-@Profile("!prod")
+@Profile({"default", "local", "stage"})
 @RequiredArgsConstructor
 public class OpenSearchLoadTestScheduler {
 
