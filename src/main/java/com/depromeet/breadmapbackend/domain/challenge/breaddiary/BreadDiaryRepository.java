d91a82470daf8abbd3838ee0e089c3ba54eb15e7
facewise <dydgus.k@gmail.com>
2024-03-22 17:32:01 +0900

    chore: 빵일기 쓰기 작업 구조

---
+++ src/main/java/com/depromeet/breadmapbackend/domain/challenge/breaddiary/BreadDiaryRepository.java
@@ -0,0 +1,6 @@
+package com.depromeet.breadmapbackend.domain.challenge.breaddiary;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BreadDiaryRepository extends JpaRepository<BreadDiary, Long> {
+}
---
+++ src/main/java/com/depromeet/breadmapbackend/domain/challenge/breaddiary/dto/AddBreadDiaryDto.java
@@ -0,0 +1,15 @@
+package com.depromeet.breadmapbackend.domain.challenge.breaddiary.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Set;
+
+public record AddBreadDiaryDto(
+        String oAuthId,
+        MultipartFile image,
+        long bakeryId,
+        long productId,
+        int rating,
+        Set<Long> bakeryTags,
+        Set<Long> productTags) {
+}
