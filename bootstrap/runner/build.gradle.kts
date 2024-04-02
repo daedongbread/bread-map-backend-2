3dc7bea781c717f3e06bf6466ee6c8ccde4b37fe
jsween5723 <jsween5723@gmail.com>
2024-04-03 01:37:57 +0900

    build: runner 최소 의존성으로 변경

--- bootstrap/runner/build.gradle.kts
+++ bootstrap/runner/build.gradle.kts
@@ -6,5 +6,5 @@ tasks.getByName("jar") {
     enabled = false
 }
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-autoconfigure")
 }
