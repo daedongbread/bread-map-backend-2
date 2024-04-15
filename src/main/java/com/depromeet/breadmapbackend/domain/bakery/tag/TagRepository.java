6e8deb6a0416fe7d76c04d29bcf3ce734f25ec9f
facewise <dydgus.k@gmail.com>
2024-04-15 17:44:00 +0900

    tag 엔티티 삭제

--- src/main/java/com/depromeet/breadmapbackend/domain/bakery/tag/TagRepository.java
+++
@@ -1,6 +0,0 @@
-package com.depromeet.breadmapbackend.domain.bakery.tag;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
