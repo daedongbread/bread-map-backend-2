9c183d2bc767fc8fb8ee590b7fe6e3156afce070
facewise <dydgus.k@gmail.com>
2024-05-08 09:03:29 +0900

    fix: 컴파일에러 수정

--- src/main/java/com/depromeet/breadmapbackend/domain/event/domain/point/UserPointHistoryContent.java
+++ src/main/java/com/depromeet/breadmapbackend/domain/event/domain/point/UserPointHistoryContent.java
@@ -1,7 +1,4 @@
 package com.depromeet.breadmapbackend.domain.event.domain.point;
 
-import lombok.Getter;
-
-@Getter
 public record UserPointHistoryContent(int point, int grandTotalPoint, PointHistoryType type, String description) {
 }
