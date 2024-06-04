25f0b4536ff2e1097655076190614077615857c5
facewise <dydgus.k@gmail.com>
2024-06-04 15:06:09 +0900

    테스트 에러 수정

--- src/main/java/com/depromeet/breadmapbackend/global/infra/EmbeddedRedisConfig.java
+++ src/main/java/com/depromeet/breadmapbackend/global/infra/EmbeddedRedisConfig.java
@@ -17,27 +17,27 @@ import javax.annotation.PostConstruct;
 @RequiredArgsConstructor
 public class EmbeddedRedisConfig {
 
-	private static final String REDIS_DOCKER_IMAGE = "redis:6";
-	private final StringRedisTemplate redisTemplate;
+    private static final String REDIS_DOCKER_IMAGE = "redis:6";
+    private final StringRedisTemplate redisTemplate;
 
-	static {
-		GenericContainer<?> REDIS_CONTAINER =
-			new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
-				.waitingFor(Wait.forLogMessage(".*Ready to accept connections.*\\n", 1))
-				.withExposedPorts(6379);
+    static {
+        GenericContainer<?> REDIS_CONTAINER =
+                new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
+                        .waitingFor(Wait.forLogMessage(".*Ready to accept connections.*\\n", 1))
+                        .withExposedPorts(6379);
 
-		REDIS_CONTAINER.start();
-		System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
-		System.setProperty("spring.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
-	}
+        REDIS_CONTAINER.start();
+        System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
+        System.setProperty("spring.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
+    }
 
-	@PostConstruct
-	public void setUp() {
-		try {
-			redisTemplate.opsForStream()
-				.createGroup("bakery-view-event", "bakery-view-event:group");
-		} catch (Exception e) {
-			log.info("bakery-view-event:group already exists : {} ", e.getMessage());
-		}
-	}
+    @PostConstruct
+    public void setUp() {
+        try {
+            redisTemplate.opsForStream()
+                    .createGroup("bakery-view-event", "bakery-view-event:group");
+        } catch (Exception e) {
+            log.info("bakery-view-event:group already exists : {} ", e.getMessage());
+        }
+    }
 }
